c728c975bb7ba5ee6fb02ca5ac1ce535a3953fcf
uday <udaykumar2600@gmail.com>
2026-08-12 01:44:07 +0530

    add account

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cookieParser = require("cookie-parser")
-const authRouter = require("./routes/auth.routes")
 
 
 
@@ -13,7 +12,17 @@ app.use(express.json())
 // middleware cookieparser
 app.use(cookieParser())
 
+/**
+ * - Routes
+ */
+const authRouter = require("./routes/auth.routes.js")
+const accountRouter = require("./routes/account.routes.js")
+
+/**
+ * - Use Routes
+ */
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 
 module.exports = app
